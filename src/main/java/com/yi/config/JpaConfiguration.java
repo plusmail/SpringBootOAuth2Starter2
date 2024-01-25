a8f29d0de4645b35152fa0815f40e2fe70159df4
plusmail <plus4957@gmail.com>
2024-01-26 08:16:23 +0900

    Rabbitmq stomp 추가2

--- src/main/java/com/yi/config/JpaConfiguration.java
+++ src/main/java/com/yi/config/JpaConfiguration.java
@@ -103,7 +103,7 @@ public class JpaConfiguration {
     public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
 
         JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(entityManagerFactory);
+        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
         return txManager;
     }
 
